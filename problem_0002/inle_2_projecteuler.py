38ebd77acc79576be1100bc5374ca95047ece0f1
Inle Bush <inle_bush@inles-macbook-air.home>
2020-04-17 21:39:26 +0200

    inle problem2 answer

---
+++ problem_0002/inle_2_projecteuler.py
@@ -0,0 +1,16 @@
+def problem2():
+    sum = 0
+    fib1 = 1
+    fib2 = 1
+    fib3 = None
+
+    while fib2 < 4000000: 
+        if fib2%2 == 0:
+            sum += fib2
+        fib3 = fib1 + fib2
+        fib1 = fib2
+        fib2 = fib3
+        
+    return sum
+
+print(problem2())
