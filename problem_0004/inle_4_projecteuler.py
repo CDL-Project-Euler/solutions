504a77246316ed800d43be496ff8b346e08b32c8
Inle Bush <inle_bush@inles-macbook-air.home>
2020-04-18 00:47:41 +0200

    inle problem 5

---
+++ problem_0004/inle_4_projecteuler.py
@@ -0,0 +1,17 @@
+#Largest product of 3 digit numbers is 999*999=998001
+#I assume answer in form abccba
+import math
+def problem4():
+    palindrome = 0
+    for x in reversed(range(100,999)):
+        for y in reversed(range(100,999)):
+            if ispalindrome(x * y) and palindrome < x*y:
+                palindrome = x*y
+    return(palindrome)
+
+def ispalindrome(product):
+    if product//100000 == product%10 and (product//10000)%10 == (product%100)//10 and (product//1000)%10 == (product%1000)//100:
+        return True
+    return False
+
+print(problem4())
