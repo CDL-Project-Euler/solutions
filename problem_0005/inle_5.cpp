c292a205fb1b2f840004fce0c841a7ef83f94b0b
Inle Bush <inle_bush@inles-macbook-air.home>
2020-04-18 01:26:57 +0200

    Hi

---
+++ problem_0005/inle_5.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+
+bool checkprime(int value){
+    int factor = 2;
+    double sqrt = std::pow(value, 0.5);
+    while(factor <= sqrt){
+        if (value % factor == 0){
+            return(false);
+        }
+        factor ++;
+    }
+    return(true);
+}
+
+int smallest_divisible(int max){
+    int product = 1;
+    for (int i = 2; i <= max; i++)
+    {
+        if (checkprime(i))
+        {
+            int power = 1;
+            while (power <= max)
+            {
+                power *= i;
+                std::cout << power;
+            }  
+            product *= power;
+        } 
+    }  
+    return(product);
+}
+
+int main(){
+    std::cout << smallest_divisible(20);
+}
