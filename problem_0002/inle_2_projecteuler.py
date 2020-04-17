f89a8c20e4d60e78ea1ca5d821cc5bb9a0aad8a2
Komron <komron_aripov@cdl.ch>
2020-04-18 01:11:35 +0200

    fix linter errors

--- problem_0002/inle_2_projecteuler.py
+++ problem_0002/inle_2_projecteuler.py
@@ -4,13 +4,14 @@ def problem2():
     fib2 = 1
     fib3 = None
 
-    while fib2 < 4000000: 
-        if fib2%2 == 0:
+    while fib2 < 4000000:
+        if fib2 % 2 == 0:
             sum += fib2
         fib3 = fib1 + fib2
         fib1 = fib2
         fib2 = fib3
-        
+
     return sum
+
 
 print(problem2())
