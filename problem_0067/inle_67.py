cd46b921c59298f4531849492c2b02a940971b0f
Inle Bush <inle_bush@inles-macbook-air.home>
2020-04-19 00:08:21 +0200

    inle_18 and 67

---
+++ problem_0067/inle_67.py
@@ -0,0 +1,24 @@
+def get_array(file_name: str):
+    with open(file_name) as f:
+        array = [[int(x) for x in line.split()] for line in f]
+    return(array)
+
+
+def max_path_sum(array):
+    for y_pos in range(1, len(array)):
+        length = len(array[y_pos])
+        for x_pos in range(length):
+            if x_pos == 0:
+                array[y_pos][0] += array[y_pos - 1][0]
+            elif x_pos == length - 1:
+                array[y_pos][x_pos] += array[y_pos - 1][-1]
+            else:
+                array[y_pos][x_pos] += max(array[y_pos-1][x_pos], array[y_pos-1][x_pos-1])
+    return max(array[-1])
+
+
+if __name__ == "__main__":
+    array = get_array("p067_triangle.txt")
+    
+    maximum = max_path_sum(array)
+    print(maximum)
