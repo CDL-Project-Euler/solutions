a876662cd3c5d8be195e44e9f32a95e7cc5010bc
Inle Bush <inle_bush@inles-macbook-air.home>
2020-04-18 18:09:29 +0200

    check

---
+++ problem_0014/inle_14.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+int length_collatz(int number)
+{
+    int count = 1;
+    std::cout << number;
+    while (number != 1)
+    {
+        if(number % 2 == 0)
+        {
+            number = number/2;
+        } 
+        else 
+        {
+            number = 3 * number - 1;
+        }
+        count ++;
+    } 
+    return count;
+}
+
+
+int longest_collatz(int max) 
+{
+    int length;
+    int longest;
+    int current_len;
+    std::cout << max;
+    for (int start = 2; start < max; start++)
+    {
+        current_len = length_collatz(start);
+
+        std::cout << "this happened" << start;
+
+        if (length < current_len)
+        {
+            length = current_len;
+            longest = start;
+        }
+    }
+    return longest;
+}
+
+int main(){
+    std::cout << longest_collatz(1000000);
+}
