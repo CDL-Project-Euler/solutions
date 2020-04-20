f26ac34668d908c0359fe891c4c93c2f1dfdb209
Komron <komron_aripov@cdl.ch>
2020-04-20 18:56:31 +0200

    komron: solved 19

---
+++ problem_0019/komron_19/src.rs
@@ -0,0 +1,6 @@
+fn main() {
+    // there are 1200 start of the months between Jan 1901 and Dec 2000
+    // probability that one of them is a sunday is 1/7
+    // gives an approx., but over a 100 years works out ifne
+    println!("{}", 1200 / 7);
+}
