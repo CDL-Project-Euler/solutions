e5e84e3b72f1321eac092719d122e3324294c3d4
Inle Bush <inle_bush@inles-macbook-air.home>
2020-04-21 15:26:15 +0200

    inle_22

---
+++ problem_0022/inle_22.py
@@ -0,0 +1,15 @@
+def get_array(file_name: str):
+    with open(file_name) as f:
+        array = [x for x in f.read()[1:-1].split('","')]
+    return(array)
+
+def index_x_letter_sum(name_list: list):
+    total = 0
+    name_list.sort()
+    for index in range(0, len(name_list)):
+        total += sum(map(lambda x: ord(x) - 64, name_list[index])) * (index+1)
+    return total
+
+if __name__ == "__main__":
+    print(index_x_letter_sum(get_array("p022_names.txt")))
+
