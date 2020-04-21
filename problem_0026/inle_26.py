acc78bd6d04de5fe74a24d20c40b09993a87ab41
Inle Bush <inle_bush@inles-macbook-air.home>
2020-04-21 14:20:30 +0200

    inle_26

--- problem_0026/inle_26.py
+++ problem_0026/inle_26.py
@@ -1,6 +1,32 @@
-from decimal import *
-getcontext().prec = 9
-# for integer in range(1, 100):
-#     print(integer, " : ", Decimal(1)/Decimal(integer))
+import math
+import decimal
 
-print(49, " : ", Decimal(1)/Decimal(729))
+#didn't use
+def length_num_digits(number: int):
+    return(int(math.log(number, 10)))
+
+def reciprocal_cycle_len(num: int):
+    for index in range(1, 1000):
+        remainder = (10**index) % num
+        if remainder == 1:
+            return index
+        elif remainder == 0:
+            return 0  #All number with no cycle
+    return "later " #All numbers with cycles starting later
+
+def max_reciprocal_cycles(maximum: int):
+    #Not including maximum
+    longest_cycle = 0
+    longest = 0 #integer with longest_cycle
+
+    for number in range(1, maximum):
+        current_cycle = reciprocal_cycle_len(number)
+        
+        if current_cycle > longest_cycle:
+            longest = number
+            longest_cycle = current_cycle
+    return longest_cycle, longest
+
+
+if __name__ == "__main__":
+    print(max_reciprocal_cycles(1000))
