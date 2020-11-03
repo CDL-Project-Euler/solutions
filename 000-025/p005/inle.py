b076dec52a729193832861c6e3dc7703de60efe2
imbush <inlebush@gmail.com>
2020-11-03 19:43:11 +0100

    inle 5

--- 000-025/p005/inle.py
+++ 000-025/p005/inle.py
@@ -15,7 +15,7 @@ def fast_lcm(factors: list, lcm = 1) -> int:
     '''Least common multiple of integers in list, multiplied by lcm'''
     if len(factors) == 1: 
         return lcm * factors[0]
-    #Iterates function with cutting down 
+    #Iterates function with cutting down
     return fast_lcm(
                     [item/factors[0] * (not bool(item%factors[0])) + item * bool(item%factors[0]) for item in factors[1:]],
                     lcm * factors[0]
